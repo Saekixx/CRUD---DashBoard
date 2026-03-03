8b95dac041d0895304cae89b4c314a646c8bb7e9
senatino2000 <senatino>
2026-03-03 11:25:52 -0500

    Clase DataBase para la conexion con la DB - Patron Singleton

---
+++ Back/src/config/conexion.js
@@ -0,0 +1,20 @@
+import { createPool } from "mysql2/promise.js";
+
+class DataBase {
+    constructor() {
+        if(DataBase.instance) return DataBase.instance;
+
+        this.pool = createPool({
+            host: "localhost",
+            user: "root",
+            password: "",
+            database: "db_negocio",
+            waitForConnections: true,
+            connectionLimit: 10
+    })
+        DataBase.instance = this;
+    }
+}
+
+const instance = new DataBase();
+export default instance;
