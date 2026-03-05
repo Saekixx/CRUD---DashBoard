fdbad838afb2abc57cb5c319e08f87ccdf7cb0e6
Jack Leandro <leandrohernandezjackdaniel@gmail.com>
2026-03-04 20:40:21 -0500

    Endpoint para activar productos inactivos creado

--- Back/src/models/product.js
+++ Back/src/models/product.js
@@ -1,33 +1,47 @@
-import db from '../config/conexion.js';
+import db from "../config/conexion.js";
 
 class Product {
-    static async getAll() {
-        const [rows] = await db.query('CALL sp_getAllProducts');
-        return rows;
-    }
+  static async getAll() {
+    const [rows] = await db.query("CALL sp_getAllProducts");
+    return rows;
+  }
 
-    static async getAllInactivos() {
-        const [rows] = await db.query('CALL sp_getAllProductsInactivos');
-        return rows;
-    }
+  static async getAllInactivos() {
+    const [rows] = await db.query("CALL sp_getAllProductsInactivos");
+    return rows;
+  }
 
-    static async getById(id) {
-        const [rows] = await db.query('CALL sp_getByIdProduct(?)', [id]);
-        return rows[0];
-    }
+  static async getById(id) {
+    const [rows] = await db.query("CALL sp_getByIdProduct(?)", [id]);
+    return rows[0];
+  }
 
-    static async create(name, description, stock, id_categoria) {
-        return await db.query('CALL sp_createProduct(?, ?, ?, ?)', [name, description, stock, id_categoria]);
-    }
+  static async create(name, description, stock, id_categoria) {
+    return await db.query("CALL sp_createProduct(?, ?, ?, ?)", [
+      name,
+      description,
+      stock,
+      id_categoria,
+    ]);
+  }
 
-    static async update(id, name, description, stock, id_categoria) {
-        return await db.query('CALL sp_updateProduct(?, ?, ?, ?, ?)', [id, name, description, stock, id_categoria]);
-    }
+  static async update(id, name, description, stock, id_categoria) {
+    return await db.query("CALL sp_updateProduct(?, ?, ?, ?, ?)", [
+      id,
+      name,
+      description,
+      stock,
+      id_categoria,
+    ]);
+  }
 
-    static async delete(id) {
-        return await db.query('CALL sp_deleteProduct(?)', [id]); 
-    }
+  static async delete(id) {
+    return await db.query("CALL sp_deleteProduct(?)", [id]);
+  }
 
+  static async activate(id) {
+    return await db.query("CALL sp_activateProduct(?)", [id]);
+  }
 }
 
 export default Product;
--- Back/src/routes/product.route.js
+++ Back/src/routes/product.route.js
@@ -1,13 +1,14 @@
-import { Router } from 'express';
-import ProductController from '../controllers/product.js';
+import { Router } from "express";
+import ProductController from "../controllers/product.js";
 
 const ProductRouter = Router();
 
-ProductRouter.get('/', ProductController.getAllProducts);
-ProductRouter.get('/inactivos', ProductController.getAllProductsInactivos);
-ProductRouter.get('/:id', ProductController.getProductById);
-ProductRouter.post('/', ProductController.createProduct);
-ProductRouter.put('/:id', ProductController.updateProduct);
-ProductRouter.delete('/:id', ProductController.deleteProduct);
+ProductRouter.get("/", ProductController.getAllProducts);
+ProductRouter.get("/inactivos", ProductController.getAllProductsInactivos);
+ProductRouter.get("/:id", ProductController.getProductById);
+ProductRouter.post("/", ProductController.createProduct);
+ProductRouter.put("/:id", ProductController.updateProduct);
+ProductRouter.delete("/:id", ProductController.deleteProduct);
+ProductRouter.post("/activate/:id", ProductController.activateProduct);
 
 export default ProductRouter;
